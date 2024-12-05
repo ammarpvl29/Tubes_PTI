55ac2eebc8cb5af04a07702229128e338726e00c
ammarpvl29 <ammar.pavel@gmail.com>
2024-12-05 23:38:59 +0700

    changed mc, change enemy

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float jumpForce = 10f; // Added jumpForce variable
+    [SerializeField] private float jumpForce = 5f; // Added jumpForce variable
     [SerializeField] private LayerMask groundLayer; // Layer for ground detection
     [SerializeField] private float groundCheckRadius = 0.2f; // Radius for ground check circle
 
@@ -12,12 +12,14 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = true;
     private bool isGrounded;
     private Transform groundCheck; // Reference to ground check point
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         originalScale = transform.localScale;
-        
+        animator = GetComponent<Animator>(); // Add Animator reference
+
         // Create and setup ground check point
         groundCheck = new GameObject("GroundCheck").transform;
         groundCheck.parent = transform;
@@ -26,16 +28,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Check if player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        animator.SetBool("IsGrounded", isGrounded);
+
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
 
-        // Handle jumping - only allow jump when grounded
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetTrigger("Jump");
+            Debug.Log("Jump Triggered");
         }
 
         // Check direction and flip if necessary
