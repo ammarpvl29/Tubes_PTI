f4a81f231f4a25b1c6b17947d7d0bd1af03703f3
fachry-kr <fachrykaer@gmail.com>
2025-01-09 16:15:38 +0700

    intro2

--- Assets/Scripts/intro2.cs
+++ Assets/Scripts/intro2.cs
@@ -1,19 +1,20 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Untuk Scene Management
+using UnityEngine.SceneManagement; // Tambahkan ini
 
-public class Intro2 : MonoBehaviour
+public class intro2 : MonoBehaviour
 {
-    public float waitTime = 22f; // Waktu tunggu sebelum pindah ke MainMenu
+    public float waitTime = 30f; // Waktu tunggu sebelum pindah ke MainMenu
 
     void Start()
     {
         StartCoroutine(WaitAndLoadMainMenu());
-    }
+    }  
 
     IEnumerator WaitAndLoadMainMenu()
     {
         yield return new WaitForSeconds(waitTime);
-        SceneManager.LoadScene("Game2"); // Pastikan nama scene sesuai dengan Build Settings
+        SceneManager.LoadScene("Game3"); // Dari intro ke 2 lanjut level 3
     }
 }
